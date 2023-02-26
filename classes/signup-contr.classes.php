35d20fe66b74a4777eb140845444812f37c61b56
gbass787 <4551@holbertonstudents.com>
2023-02-26 18:12:13 -0400

    Update signup-contr.classes.php

--- classes/signup-contr.classes.php
+++ classes/signup-contr.classes.php
@@ -3,54 +3,115 @@
 class SignupContr extends Signup {
 
     private $uid;
-    private $email;
     private $pwd;
-
-    public function __construct($uid, $email, $pwd) {
+    private $pwdRepeat;
+    private $email;
+    
+    public function __construct($uid, $pwd, $pwdRepeat, $email) {
         $this->uid = $uid;
-        $this->email = $email;
         $this->pwd = $pwd;
+        $this->pwdRepeat = $pwdRepeat;
+        $this->email = $email;
     }
 
     public function signupUser() {
-        try {
-            $userExists = $this->checkUser($this->uid, $this->email);
-        } catch (Exception $e) {
-            header("location: ../index.php?error=" . $e->getMessage());
+        if($this->emptyInput() == false) {
+            // echo "Empty input!";
+            header("location: ../index.php?error=emptyinput");
+            exit();
+        }
+        if($this->invalidUid() == false) {
+            // echo "Invalid username!";
+            header("location: ../index.php?error=username");
+            exit();
+        }
+        if($this->invalidEmail() == false) {
+            // echo "Invalid email!";
+            header("location: ../index.php?error=email");
+            exit();
+        }
+        if($this->pwdMatch() == false)
+        {
+            // echo "Passwords don't match!";
+            header("location: ../index.php?error=passwordmatch");
+            exit();
+        }
+        if($this->uidTakenCheck() == false)
+        {
+            // echo "Username or email taken!";
+            header("location: ../index.php?error=useroremailtaken");
             exit();
         }
 
-        if($userExists) {
-            try {
-                $this->setUser($this->uid, $this->pwd, $this->email);
-            } catch (Exception $e) {
-                header("location: ../index.php?error=" . $e->getMessage());
-                exit();
-            }
-
-            header("location: ../index.php?signup=success");
-            exit();
-        } else {
-            header("location: ../index.php?error=usertaken");
-            exit();
-        }
+        $this->setUser($this->uid, $this->pwd, $this->email);
     }
 
-    public function uidTakenCheck() {
-        try {
-            $userExists = $this->checkUser($this->uid, $this->email);
-        } catch (Exception $e) {
-            header("location: ../signup.php?error=" . $e->getMessage());
-            exit();
+    private function emptyInput() {
+        $result;
+        if(empty($this->uid) || empty($this->pwd) || empty($this->pwdRepeat) || empty($this->email)) {
+            $result = false;
         }
+        else {
+            $result = true;
+        }
+        return $result;
+    }
 
-        if($userExists) {
-            header("location: ../signup.php?signup=uidavailable");
-            exit();
-        } else {
-            header("location: ../signup.php?error=usertaken");
-            exit();
+    private function invalidUid() {
+        $result;
+        if (!preg_match("/^[a-zA-Z0-9]*$/", $this->uid)) 
+        {
+            $result = false;
         }
+        else 
+        {
+            $result = true;
+        }
+        return $result;
+    }
+
+    private function invalidEmail() {
+        $result;
+        if (!filter_var($this->email, FILTER_VALIDATE_EMAIL)) 
+        {
+            $result = false;
+        }
+        else 
+        {
+            $result = true;
+        }
+        return $result;
+    }
+
+    private function pwdMatch() {
+        $result;
+        if ($this->pwd !== $this->pwdRepeat) 
+        {
+            $result = false;
+        }
+        else 
+        {
+            $result = true;
+        }
+        return $result;
+    }
+
+    private function uidTakenCheck() {
+        $result;
+        if (!$this->checkUser($this->uid, $this->email)) 
+        {
+            $result = false;
+        }
+        else 
+        {
+            $result = true;
+        }
+        return $result;
+    }
+
+    public function fetchUserId($uid) {
+        $userId = $this->getUserId($uid);
+        return $userId[0]["users_id"];
     }
 
 }
